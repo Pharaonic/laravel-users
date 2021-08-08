ee4681e15e3b1c92c4fbc582ee20afd0320f6368
Moamen Eltouny <support@raggitech.com>
2021-08-08 14:10:33 +0200

    fix migrations

--- src/UsersServiceProvider.php
+++ src/UsersServiceProvider.php
@@ -31,11 +31,13 @@ class UsersServiceProvider extends ServiceProvider
         $this->publishes([
         //     __DIR__ . '/views'  => resource_path('views/vendor/laravel-menus'),
 
-            __DIR__ . '/database/migrations/2021_02_01_000019_create_devices_table.php'             => database_path('migrations/2021_02_01_000019_create_devices_table.php'),
-            __DIR__ . '/database/migrations/2021_02_01_000020_create_operation_systems_table.php'   => database_path('migrations/2021_02_01_000020_create_operation_systems_table.php'),
-            __DIR__ . '/database/migrations/2021_02_01_000021_create_browsers_table.php'            => database_path('migrations/2021_02_01_000021_create_browsers_table.php'),
-            __DIR__ . '/database/migrations/2021_02_01_000022_create_bots_table.php'                => database_path('migrations/2021_02_01_000022_create_bots_table.php'),
-            __DIR__ . '/database/migrations/2021_02_01_000023_create_agents_table.php'              => database_path('migrations/2021_02_01_000023_create_agents_table.php'),
+            __DIR__ . '/database/migrations/2021_02_01_000019_create_devices_table.php'                 => database_path('migrations/2021_02_01_000019_create_devices_table.php'),
+            __DIR__ . '/database/migrations/2021_02_01_000020_create_operation_systems_table.php'       => database_path('migrations/2021_02_01_000020_create_operation_systems_table.php'),
+            __DIR__ . '/database/migrations/2021_02_01_000021_create_browsers_table.php'                => database_path('migrations/2021_02_01_000021_create_browsers_table.php'),
+            __DIR__ . '/database/migrations/2021_02_01_000022_create_bots_table.php'                    => database_path('migrations/2021_02_01_000022_create_bots_table.php'),
+            __DIR__ . '/database/migrations/2021_02_01_000023_create_agents_table.php'                  => database_path('migrations/2021_02_01_000023_create_agents_table.php'),
+            __DIR__ . '/database/migrations/2021_02_01_000024_create_user_agents_table.php'             => database_path('migrations/2021_02_01_000024_create_user_agents_table.php'),
+            __DIR__ . '/database/migrations/2021_02_01_000025_create_user_password_history_table.php'   => database_path('migrations/2021_02_01_000025_create_user_password_history_table.php'),
         ], ['pharaonic', 'laravel-users']);
 
         // Blade - Directive
